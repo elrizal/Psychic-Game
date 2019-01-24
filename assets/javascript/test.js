64202c1ae726cd98795af23a8978d045613035ae
elrizal <erinrizal@Erins-MacBook-Pro.local>
2019-01-23 23:28:54 -0500

    new

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,12 +1,3 @@
-//  Essentially, the app randomly picks a letter, and the user has to guess which letter the app chose.
-//Guess what letter I'm thinking of
-//Wins: (# of times the user has guessed the letter correctly)
-//Psuedo-code: Losses= (# of times the user has failed to guess the letter correctly after exhausting all guesses)**
-//Guesses Left= (# of guesses left. This will update)**
-//Guesses So Far=(the specific letters that the user typed. Display these until the user either wins or loses.)
-// player wins, increase the Wins counter and start the game over again (without refreshing the page).
-// player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
- 
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "y", "w", "x", "z"];
     
     var wins = 0;
